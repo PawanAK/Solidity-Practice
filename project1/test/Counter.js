33c7ec0f2227d57bf6ceb1c05dbe44894885596d
PawanAK <pawanajjark@gmail.com>
2023-10-24 20:33:14 +0530

    Testing Done

--- project1/test/Counter.js
+++ project1/test/Counter.js
@@ -1,2 +1,36 @@
+const { ethers } = require('hardhat');
 const { expect } = require('chai');
-const { ether } = require('hardhat');
+
+describe('Counter', function () {
+    let Counter;
+    let counter;
+    let owner;
+
+    before(async function () {
+        [owner] = await ethers.getSigners();
+
+        const CounterFactory = await ethers.getContractFactory('Counter');
+        Counter = await CounterFactory.deploy('MyCounter', 0);
+        await Counter.deployed();
+
+        counter = Counter;
+    });
+
+    it('should initialize with the correct name and count', async function () {
+        expect(await counter.name()).to.equal('MyCounter');
+        expect(await counter.count()).to.equal(0);
+    });
+
+    it('should increment and decrement the count', async function () {
+        await counter.increment();
+        expect(await counter.count()).to.equal(1);
+
+        await counter.decrement();
+        expect(await counter.count()).to.equal(0);
+    });
+
+    it('should update the name', async function () {
+        await counter.setName('NewCounterName');
+        expect(await counter.name()).to.equal('NewCounterName');
+    });
+});
